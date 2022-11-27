b2318d18a3ae270a114f5d0ebda6cac4583f92fd
Matheus Passos <msena789@gmail.com>
2022-11-27 19:59:51 -0300

    exportando dados com bridge - exportando conteudo

---
+++ src/Relatorio/OrcamentoExportado.php
@@ -0,0 +1,26 @@
+<?php
+
+
+namespace Alura\DesignPattern\Relatorios;
+
+
+use Alura\DesignPattern\Orcamento;
+
+class OrcamentoExportado implements ConteudoExportado
+{
+	
+	private Orcamento $orcamento;
+	
+	public function __construct(Orcamento $orcamento)
+	{
+		$this->orcamento = $orcamento;
+	}
+	
+	public function conteudo(): array
+	{
+		return [
+			'valor' => $this->orcamento->valor,
+			'quantidade_itens' => $this->orcamento->quantidadeItens,
+		];
+	}
+}
