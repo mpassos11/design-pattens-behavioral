6ebb13f4da4c93fe6c771559ec77619ed9bd661e
Matheus Passos <msena789@gmail.com>
2022-11-27 18:43:30 -0300

    iterator - representando uma coleção de orçamentos

--- lista-orcamentos.php
+++ lista-orcamentos.php
@@ -2,6 +2,7 @@
 
 require 'vendor/autoload.php';
 
+use Alura\DesignPattern\ListaDeOrcamentos;
 use Alura\DesignPattern\Orcamento;
 
 $orcamento1 = new Orcamento();
@@ -20,9 +21,12 @@ $orcamento3->valor = 1350;
 $orcamento3->aprova();
 $orcamento3->finaliza();
 
-$listaOrcamentos = [$orcamento1, $orcamento2, $orcamento3, 'string aleatoria que quebra o codigo'];
+$listaOrcamentos = new ListaDeOrcamentos();
+$listaOrcamentos->addOrcamento($orcamento1);
+$listaOrcamentos->addOrcamento($orcamento2);
+$listaOrcamentos->addOrcamento($orcamento3);
 
-foreach ($listaOrcamentos as $orcamento) {
+foreach ($listaOrcamentos->getOrcamentos() as $orcamento) {
 	echo "Valor: {$orcamento->valor}" . PHP_EOL;
 	echo "Estado: ". get_class($orcamento->estadoAtual) . PHP_EOL;
 	echo "Qtd. Itens: {$orcamento->quantidadeItens}" . PHP_EOL;
---
+++ src/ListaDeOrcamentos.php
@@ -0,0 +1,28 @@
+<?php
+
+
+namespace Alura\DesignPattern;
+
+
+class ListaDeOrcamentos
+{
+	/**
+	 * @var Orcamento[]
+	 */
+	private array $orcamentos;
+	
+	public function __construct()
+	{
+		$this->orcamentos = [];
+	}
+	
+	public function addOrcamento(Orcamento $orcamento)
+	{
+		$this->orcamentos[] = $orcamento;
+	}
+	
+	public function getOrcamentos() : array
+	{
+		return $this->orcamentos;
+	}
+}
