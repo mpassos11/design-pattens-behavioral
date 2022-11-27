a74891ef1d0246856c81b7af22d60cb415c03ab1
Matheus Passos <msena789@gmail.com>
2022-11-27 19:42:25 -0300

    adapters para reutilizar dependencias - criando um adapter

--- src/RegistroOrcamento.php
+++ src/RegistroOrcamento.php
@@ -4,10 +4,27 @@
 namespace Alura\DesignPattern;
 
 
+use Alura\DesignPattern\EstadosOrcamento\Finalizado;
+use Alura\DesignPattern\Http\HttpAdapter;
+
 class RegistroOrcamento
 {
+	private HttpAdapter $httpAdapter;
+	
+	public function __construct(HttpAdapter $httpAdapter)
+	{
+		$this->httpAdapter = $httpAdapter;
+	}
+	
 	public function registrar(Orcamento $orcamento) : void
 	{
-	
+		if (!$orcamento->estadoAtual instanceof Finalizado) {
+			throw new \DomainException('Apenas orçamentos finalizados podem ser registrados na API');
+		}
+		
+		$this->httpAdapter->post("http://api.registrar.orcamento", [
+			'valor' => $orcamento->valor,
+			'quantidadeItens' => $orcamento->quantidadeItens
+		]);
 	}
 }
