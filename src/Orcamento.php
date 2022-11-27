ed409f718050dda2c02ae7bbbc19cb3755c922f7
Matheus Passos <msena789@gmail.com>
2022-11-27 17:22:24 -0300

    state - extraindo classe de estado

--- src/Orcamento.php
+++ src/Orcamento.php
@@ -4,27 +4,37 @@
 namespace Alura\DesignPattern;
 
 
+use Alura\DesignPattern\EstadosOrcamento\EmAprovacao;
+use Alura\DesignPattern\EstadosOrcamento\EstadoOrcamento;
+
 class Orcamento
 {
 	public int $quantidadeItens;
 	public float $valor;
-	public string $estadoAtual;
+	public EstadoOrcamento $estadoAtual;
+	
+	public function __construct()
+	{
+		$this->estadoAtual = new EmAprovacao();
+	}
 	
 	public function aplicaDescontoExtra()
 	{
-		$this->valor -= $this->calculaDescontoExtra();
+		$this->valor -= $this->estadoAtual->calculaDescontoExtra($this);
 	}
 	
-	public function calculaDescontoExtra() : float
+	public function aprova()
 	{
-		if ($this->estadoAtual == 'EM_APROVACAO') {
-			return $this->valor * 0.05;
-		}
-		
-		if ($this->estadoAtual == 'APROVADO') {
-			return $this->valor * 0.02;
-		}
-		
-		throw new \DomainException('Orçamentos reprovados ou finalizados não podem receber desconto extra');
+		$this->estadoAtual->aprova($this);
+	}
+	
+	public function reprova()
+	{
+		$this->estadoAtual->reprova($this);
+	}
+	
+	public function finaliza()
+	{
+		$this->estadoAtual->finaliza($this);
 	}
 }
